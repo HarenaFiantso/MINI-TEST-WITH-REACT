3329e93835fe26eff7ca1c1710bf89b1f3644be0
Fiantso Harena <fiantsorav@gmail.com>
2023-07-04 19:57:25 +0300

    Feat: Make the mouseOver function

--- EVENTS/src/components/MyCars.jsx
+++ EVENTS/src/components/MyCars.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import Cars from "./Cars";
+import "./MyCars.css"
 
 class MyCars extends Component {
 	noCopy = () => {
 		alert("Ehhh Maty aaaa!")
 	}
+	mouseOver = (e) => {
+		if (e.target.classList.contains('styled')) {
+			e.target.classList.remove('styled');	
+		} else {
+			e.target.classList.add('styled');
+		}
+	}
+
 	render() {
 		return (
 			<div>
-				<h1>{this.props.title}</h1>
-				<p onCopy={this.noCopy}>Tsy azo ipipizana eto</p>
+				<h1 onMouseOver={this.mouseOver}>{this.props.title}</h1>
+				<p onCopy={this.noCopy}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, sint.</p>
 				<Cars color="Rouge">Toyota</Cars>
 				<Cars>Aston Martin</Cars>
 				<Cars color="Noire"></Cars>
