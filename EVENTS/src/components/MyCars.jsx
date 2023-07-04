2df20903f92d8e5e784371a98710128267cdf6f2
Fiantso Harena <fiantsorav@gmail.com>
2023-07-04 19:49:27 +0300

    Fix: Make the onCopy function

--- EVENTS/src/components/MyCars.jsx
+++ EVENTS/src/components/MyCars.jsx
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 import Cars from "./Cars";
 
 class MyCars extends Component {
+	noCopy = () => {
+		alert("Ehhh Maty aaaa!")
+	}
 	render() {
 		return (
 			<div>
 				<h1>{this.props.title}</h1>
+				<p onCopy={this.noCopy}>Tsy azo ipipizana eto</p>
 				<Cars color="Rouge">Toyota</Cars>
 				<Cars>Aston Martin</Cars>
 				<Cars color="Noire"></Cars>
